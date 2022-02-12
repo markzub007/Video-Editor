da60095717126fe2191e4353550109483cbf6a34
Mark <73854487+markzub007@users.noreply.github.com>
2022-02-11 21:52:13 -0800

    Create index.ts
    This is just a basic overview of the application supports

---
+++ index.ts
@@ -0,0 +1,64 @@
+class MediaFile {
+  lengthInSeconds: number;
+  soundVolumeInPercentage: number;
+}
+
+class VideoFile extends MediaFile {
+  file: any;
+}
+
+class AudioFile extends MediaFile {
+  file: any;
+}
+
+class ImageFile {
+  file: any;
+}
+
+class Interval {
+  startTimeInSeconds: number;
+  endTimeInSeconds: number;
+}
+
+class VideoEditor {
+  private _videoFile: VideoFile;
+  constructor(videoFiles: VideoFile[], audioFiles: AudioFile[] = []) {
+    this._videoFile = this.mergeMedias(videoFiles, audioFiles);
+  }
+
+  /**
+   * This function will merge the media files in order that's given
+   * @param videoFiles A list of video files in order
+   * @param audioFiles A list of audio files in order
+   */
+  private mergeMedias(
+    videoFiles: VideoFile[],
+    audioFiles: AudioFile[] = []
+  ): VideoFile {}
+
+  /**
+   * This function will remove the list of interval provided in the _videoFile
+   * @param splitIntervals A list of intervals that needs to be removed from the _video
+   */
+  public split(splitIntervals: Interval[]): VideoFile {}
+
+  /**
+   * This function will add a image overlay into _video
+   * @param image image overlay file
+   * @param topLeftPosition image top left position in the video frame
+   * @param topRightPosition image top right position in the video frame
+   * @param bottomLeftPosition image bottom left position in the video frame
+   * @param bottomRightPosition image bottom right position in the video frame
+   * @param startTimeInSeconds when the image display in the video
+   * @param endTimeInSeconds when the image stop display 
+   * */
+  public imageOverlay(
+    image: ImageFile,
+    topLeftPosition: number,
+    topRightPosition: number,
+    bottomLeftPosition: number,
+    bottomRightPosition: number,
+    startTimeInSeconds: number
+    endTimeInSeconds: number
+  ): VideoFile {}
+}
